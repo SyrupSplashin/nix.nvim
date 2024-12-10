335a23cb17cac95b15a61b87e26eb39152b69348
SyrupSplashin <vertex@syrupsplash.in>
2024-12-10 13:38:43 -0500

    Trying to get qmk plugin to work / re-figure out this weird flake situation; it's been awhie

--- nvim/plugin/qmk.lua
+++ nvim/plugin/qmk.lua
@@ -1,14 +1,10 @@
-return {
-    'codethread/qmk.nvim',
-    config = function()
-        require('qmk').setup{
-            name = 'LAYOUT_split_3x6_3',
-            layout = {
-                '_ x x x x x x _ x x x x x x _',
-                '_ x x x x x x _ x x x x x x _',
-                '_ x x x x x x _ x x x x x x _',
-                '_ _ _ _ x x x _ x x x _ _ _ _',
-            },
-        }
-    end,
+local qmk = require 'qmk'
+qmk.setup {
+    name = 'LAYOUT_split_3x6_3',
+    layout = {
+        '_ x x x x x x _ x x x x x x _',
+        '_ x x x x x x _ x x x x x x _',
+        '_ x x x x x x _ x x x x x x _',
+        '_ _ _ _ x x x _ x x x _ _ _ _',
+    },
 }
